c9ef4f507d5da39cac5da609d51f6a4c74af5cf1
Emilie-thp <emilie.marjollet@orange.fr>
2019-11-26 10:46:02 +0100

    view show of cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,7 @@
-class CartController < ApplicationController
+class CartsController < ApplicationController
+  
   def show
+  	@cart = Cart.find(params[:id])
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'cart/show'
-  devise_for :users
   root to: "items#index"
+  resources :carts
+  devise_for :users
   resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
