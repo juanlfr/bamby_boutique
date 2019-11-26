fe757b988170b5d07ad1b16a417a0a4d8463f84a
kwouix <emma.bock49@gmail.com>
2019-11-26 17:55:17 +0100

    created users controller, modified footer partial, created user show profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: "items#index"
-  resources :carts
   devise_for :users
+  resources :carts
+  resources :users, only: [:show]
   resources :items
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
