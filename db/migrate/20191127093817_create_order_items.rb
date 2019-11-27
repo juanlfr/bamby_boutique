d650471c09ce1e726ff77b717e315e6f03a098e5
Emilie-thp <emilie.marjollet@orange.fr>
2019-11-27 17:57:17 +0100

    first step order_creation

--- db/migrate/20191127093817_create_order_items.rb
+++ db/migrate/20191127093817_create_order_items.rb
@@ -3,6 +3,7 @@ class CreateOrderItems < ActiveRecord::Migration[5.2]
     create_table :order_items do |t|
       t.belongs_to :order
       t.belongs_to :item 
+    	t.integer :quantity
       t.timestamps
     end
   end
