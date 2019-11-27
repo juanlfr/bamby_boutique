531e93b54c18dd489e2d99cb97ceacf5835693bb
Emilie-thp <emilie.marjollet@orange.fr>
2019-11-27 18:51:55 +0100

    test heroku stripe

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,26 +14,7 @@ before_action :authenticate_user!
   end
 
   def create
-  	@order = Order.new
-  	@order.user = current_user
-  	@order.save
-  	current_user.cart.line_items.each do |line_item|
-  		order_items_transfer(line_item, @order)
-  	end
-  	redirect_to root_path
-
   end
 
-
- 	private 
-
- 	def order_items_transfer(line_item, current_order)		
- 		line_order = OrderItem.new
- 		line_order.order = current_order
- 		line_order.item = line_item.item
- 		line_order.quantity = line_item.quantity 
- 		line_order.save
- 		line_item.destroy
- 	end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,13 @@ class Order < ApplicationRecord
   has_many :order_items
   has_many :items, through: :order_items
   belongs_to :user
+
+  def total_order
+    sum = 0
+    self.order_items.each do |order_item|
+      sum+= order_item.total_by_item
+    end
+    return sum
+  end
+
 end
