e97313bc02f6bd7c3b0d480b3466fc3609e2d6ad
Emilie-thp <55739570+Emilie-thp@users.noreply.github.com>
2019-11-28 11:34:34 +0100

    Update devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -297,4 +297,3 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 end
-_item
